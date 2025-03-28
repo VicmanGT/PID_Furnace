522058cb20df72b5989ac9c79ac95c8d23c36dae
My name <me@example.com>
2025-03-28 09:55:54 -0600

    created project

---
+++ Debug/Src/main.d
@@ -0,0 +1,8 @@
+Src/main.o: ../Src/main.c C:/ST/STM32F051R8T6/stm32f0xx.h \
+ C:/ST/STM32F051R8T6/core_cm0.h C:/ST/STM32F051R8T6/core_cmInstr.h \
+ C:/ST/STM32F051R8T6/core_cmFunc.h C:/ST/STM32F051R8T6/system_stm32f0xx.h
+C:/ST/STM32F051R8T6/stm32f0xx.h:
+C:/ST/STM32F051R8T6/core_cm0.h:
+C:/ST/STM32F051R8T6/core_cmInstr.h:
+C:/ST/STM32F051R8T6/core_cmFunc.h:
+C:/ST/STM32F051R8T6/system_stm32f0xx.h:
